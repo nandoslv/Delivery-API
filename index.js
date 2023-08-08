a2643fe779eb7fd7296445730307102bce8a4085
Fernando Silva <119902543+nandoslv@users.noreply.github.com>
2023-08-07 21:04:15 -0300

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,51 @@
+import express from 'express';
+import pedidosRouter from './routes/pedidos.js';
+import maisVendidosRouter from './routes/maisVendidos.js';
+import { promises as fs } from 'fs';
+import winston from 'winston';
+import cors from 'cors';
+
+const { readFile, writeFile } = fs;
+const { combine, timestamp, label, printf } = winston.format;
+const myFormat = printf(({level, message, label, timestamp}) => {
+    return `${timestamp} [${label}] ${level}: ${message}`;
+});
+
+global.fileName = 'pedidos.json';
+global.logger = winston.createLogger({
+    level: 'silly',
+    transports: [
+        new (winston.transports.Console)(),
+        new (winston.transports.File)({ filename: 'delivery-api.log' })
+    ],
+    format: combine(
+        label({ label: 'delivery-api' }),
+        timestamp(),
+        myFormat
+    )
+});
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static('public'));
+app.use(cors());
+app.use('/pedidos', pedidosRouter);
+app.use('/maisVendidos/', maisVendidosRouter);
+
+app.listen(3000, async () => {
+    try {
+        await readFile(global.fileName);
+        global.logger.info("API Started!")
+    } catch (err) {
+        const initialJson = {
+            nextId: 1,
+            accounts: []
+        };
+        writeFile(global.fileName, JSON.stringify(initialJson)).then(() => {
+            global.logger.info('Delivery-API Started and File Created!')
+        }).catch(err => {
+            global.logger.error(err);
+        });
+    }
+});
---
+++ pedidos.js
@@ -0,0 +1,181 @@
+import express from "express";
+import { promises as fs } from 'fs';
+
+const { readFile, writeFile } = fs;
+const router = express.Router();
+
+router.get('/', async (req, res, next) => {
+    try {
+        const data = JSON.parse(await readFile(global.fileName));
+        delete data.nextId;
+        res.send(data);
+
+        global.logger.info(`${req.method} ${req.baseUrl}`);
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.post('/', async (req, res, next) => {
+    try {
+        let pedido = req.body;
+        const data = JSON.parse(await readFile(global.fileName));
+
+        if (!pedido.cliente || !pedido.produto || pedido.valor == null) {
+            throw new Error("Cliente, Produto e Valor são obrigatórios.");
+        }
+
+        pedido = {
+            id: data.nextId,
+            cliente: pedido.cliente,
+            produto: pedido.produto,
+            valor: pedido.valor,
+            entregue: false,
+            timestamp: new Date()
+        };
+
+        data.nextId++;
+        data.pedidos.push(pedido);
+
+        await writeFile(global.fileName, JSON.stringify(data, null, 2));
+        res.send(pedido);
+
+        global.logger.info(`${req.method} ${req.baseUrl} - Account = ${JSON.stringify(pedido)}`);
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.put('/', async (req, res, next) => {
+    try {
+        let pedido = req.body;
+
+        if (!pedido.id || !pedido.cliente || !pedido.produto || pedido.valor == null || pedido.entregue == null) {
+            throw new Error("ID, Cliente, Produto, Valor,Entregue são obrigatórios.");
+        }
+
+        const data = JSON.parse(await readFile(global.fileName));
+        const index = data.pedidos.findIndex(ac => ac.id === parseInt(pedido.id));
+
+        if (index === -1) {
+            throw new Error("Registro não encontrado!");
+        }
+
+        data.pedidos[index].cliente = pedido.cliente;
+        data.pedidos[index].produto = pedido.produto;
+        data.pedidos[index].valor = pedido.valor;
+        data.pedidos[index].entregue = pedido.entregue;
+
+        await writeFile(global.fileName, JSON.stringify(data, null, 2));
+        res.send(pedido);
+
+        global.logger.info(`${req.method} ${req.baseUrl} - Account = ${JSON.stringify(pedido)}`);
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.patch('/', async (req, res, next) => {
+    try {
+        let pedido = req.body;
+
+        if (!pedido.id || pedido.entregue == null) {
+            throw new Error("ID e Entregue são obrigatórios.");
+        }
+
+        const data = JSON.parse(await readFile(global.fileName));
+        const index = data.pedidos.findIndex(ac => ac.id === parseInt(pedido.id));
+
+        if (index === -1) {
+            throw new Error("Registro não encontrado!");
+        }
+
+        data.pedidos[index].entregue = pedido.entregue;
+
+        await writeFile(global.fileName, JSON.stringify(data, null, 2));
+        res.send(pedido);
+
+        global.logger.info(`${req.method} ${req.baseUrl} - Account = ${JSON.stringify(pedido)}`);
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.delete('/:id', async (req, res, next) => {
+    try {
+        const data = JSON.parse(await readFile(global.fileName));
+        const pedidos = data.pedidos.filter((pedido) =>
+            pedido.id !== parseInt(req.params.id)
+        );
+
+        data.pedidos = pedidos;
+        await writeFile(global.fileName, JSON.stringify(data, null, 2));
+        res.end();
+
+        global.logger.info(`${req.method} ${req.baseUrl} - ID = ${req.params.id}`);
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.get('/:id', async (req, res, next) => {
+    try {
+        const data = JSON.parse(await readFile(global.fileName));
+        const pedido = data.pedidos.filter((pedido) =>
+            pedido.id === parseInt(req.params.id)
+        );
+
+        res.send(pedido);
+
+        global.logger.info(`${req.method} ${req.baseUrl}`);
+    } catch (err) {
+        next(err);
+    }
+});
+
+
+router.post('/totalPorCliente', async (req, res, next) => {
+    try {
+        let params = req.body;
+        let totalPorCliente = 0;
+        const data = JSON.parse(await readFile(global.fileName));
+        const pedidos = data.pedidos.filter((pedido) => {
+            return pedido.cliente === params.cliente && pedido.entregue == true
+        }).map((pedido) => {
+            totalPorCliente += parseFloat(pedido.valor)
+        })
+
+        res.send(`O valor total de pedidos já realizados e entregues para o cliente ${params.cliente} é de R$${totalPorCliente}.`);
+
+        global.logger.info(`${req.method} ${req.baseUrl}`);
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.post('/totalPorProduto', async (req, res, next) => {
+    try {
+        let params = req.body;
+        let totalPorProduto = 0;
+        const data = JSON.parse(await readFile(global.fileName));
+        const pedidos = data.pedidos.filter((pedido) => {
+            return pedido.produto === params.produto && pedido.entregue == true
+        }).map((pedido) => {
+            totalPorProduto += parseFloat(pedido.valor)
+        })
+
+        res.send(`O valor total de pedidos já realizados e entregues para o produto ${params.produto} é de R$${totalPorProduto}.`);
+
+        global.logger.info(`${req.method} ${req.baseUrl}`);
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.use((err, req, res, next) => {
+    global.logger.error(`${req.method} ${req.baseUrl} - ${err.message}`);
+    console.log(err)
+    res.status(400).send({ error: err.message })
+})
+
+export default router;
